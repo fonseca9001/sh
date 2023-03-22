d5169da01ab210631b85c0383676a37a1c4a9c12
fonseca9001 <fonsecajoao@live.com>
2023-03-22 00:21:48 +0000

    Fixed with the magic of awk

--- libva.sh
+++ libva.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 # Detect GPU vendor
-gpu_vendor=$(lspci | grep -E "VGA|3D" | grep -E "Intel|NVIDIA" | head -n 1 | cut -d " " -f 5)
-
+gpu_vendor=$(glxinfo | grep -m 1 -E -i "OpenGL vendor|OpenGL renderer string" | awk '{if (tolower($0) ~ /intel/) print "Intel"; else if (tolower($0) ~ /nvidia/) print "NVIDIA"}')
 # Set LIBVA_DRIVER_NAME based on GPU vendor
 if [ "$gpu_vendor" = "Intel" ]; then
     export LIBVA_DRIVER_NAME=iHD
 elif [ "$gpu_vendor" = "NVIDIA" ]; then
     export LIBVA_DRIVER_NAME=nvidia
-fi
-
-# Check that LIBVA_DRIVER_NAME has been set
-if [[ -z "${LIBVA_DRIVER_NAME}" ]]; then
-    echo "Error: LIBVA_DRIVER_NAME not set" >> ~/libvaset.log
-    exit 1
 fi
