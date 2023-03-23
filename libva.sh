dc9f0d56e45dda9c7272dab38055d46f046806f0
fonseca9001 <fonsecajoao@live.com>
2023-03-23 09:41:16 +0000

    Reverted to original format, adding VDPAU works now

--- libva.sh
+++ libva.sh
@@ -4,7 +4,7 @@
 gpu_vendor=$(glxinfo | grep -m 1 -E -i "OpenGL vendor|OpenGL renderer string" | awk '{if (tolower($0) ~ /intel/) print "Intel"; else if (tolower($0) ~ /nvidia/) print "NVIDIA"}')
 # Set LIBVA_DRIVER_NAME based on GPU vendor
 if [ "$gpu_vendor" = "Intel" ]; then
-    export LIBVA_DRIVER_NAME=iHD
+    export LIBVA_DRIVER_NAME=iHD && export VDPAU_DRIVER=va_gl
 elif [ "$gpu_vendor" = "NVIDIA" ]; then
-    export LIBVA_DRIVER_NAME=nvidia
+    export LIBVA_DRIVER_NAME=nvidia && export VDPAU_DRIVER=nvidia
 fi
--- libvav3.sh
+++ libvav3.sh
@@ -3,24 +3,32 @@
 # Set error handling
 set -e
 
+# Set log file path
+LOG_FILE="$HOME/libvaset.log"
+
 # Check if glxinfo is installed
-command -v glxinfo >/dev/null 2>&1 || { echo >&2 "glxinfo is required but not installed. Aborting."; exit 1; }
+if ! command -v glxinfo >/dev/null 2>&1; then
+    echo "glxinfo is required but not installed. Aborting." >> "$LOG_FILE"
+    exit 1
+fi
 
 # Get GPU vendor
 gpu_vendor=$(glxinfo | grep -m 1 -E -i "OpenGL vendor|OpenGL renderer string" | awk '{if (tolower($0) ~ /intel/) print "Intel"; else if (tolower($0) ~ /nvidia/) print "NVIDIA"}')
 
-# Set LIBVA_DRIVER_NAME based on GPU vendor
+# Set LIBVA_DRIVER_NAME and VDPAU_DRIVER based on GPU vendor
 case $gpu_vendor in
     Intel)
-        export LIBVA_DRIVER_NAME=iHD
+        export LIBVA_DRIVER_NAME=iHD &&
         export VDPAU_DRIVER=va_gl
         ;;
     NVIDIA)
-        export LIBVA_DRIVER_NAME=nvidia
+        export LIBVA_DRIVER_NAME=nvidia &&
         export VDPAU_DRIVER=nvidia
         ;;
     *)
-        echo "Unsupported GPU vendor: $gpu_vendor"
+        echo "Unsupported GPU vendor: $gpu_vendor" >> "$LOG_FILE"
         exit 1
         ;;
 esac
+
+exit 0
